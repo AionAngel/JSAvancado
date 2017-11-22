c0cff10be374a74b6473a6eb1ebc1ca9fc7e9fd2
AionAngel <lucas.augustus@gmail.com>
2017-11-22 17:02:29 -0200

    Implementada a classe Bind para associar a model com a view

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -8,19 +8,18 @@ class NegociacaoController {
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
 
-        
-        this._listaNegociacoes = ProxyFactory.create(
-            new ListaNegociacoes(),
-        ['adiciona', 'esvazia'], model => this._negociacoesView.update(model));
-
-
-
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
-        
-        this._mensagem = ProxyFactory.create(
-            new Mensagem(), ['texto'], model => this._mensagemView.update(model));
+        this._listaNegociacoes = new Bind(new ListaNegociacoes(), 
+            this._negociacoesView,
+            'adiciona', 'apaga');
+            
+
 
         this._mensagemView = new MensagemView($('#mensagemView'));
+        this._mensagem = new Bind(new Mensagem(), 
+            this._mensagemView,
+            'texto');
+
         
         this._negociacoesView.update(this._listaNegociacoes);
         this._mensagemView.update(this._mensagem);
--- client/js/app/views/MensagemView.js
+++ client/js/app/views/MensagemView.js
@@ -8,10 +8,9 @@ class MensagemView extends View {
     
     template(model) {
         
-       return `<p class="alert alert-info">${model.texto}</p>`;
+       return model.texto ? `<p class="alert alert-info">${model.texto}</p>` : '<p></p>';
         
     }
-    
     
 }
 

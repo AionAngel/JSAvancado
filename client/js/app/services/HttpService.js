9d73844c650749136da2cbf322d6dcddd02d5380
AionAngel <lucas.augustus@gmail.com>
2017-12-06 14:57:25 -0200

    INsercao do metodo Fetch

--- client/js/app/services/HttpService.js
+++ client/js/app/services/HttpService.js
@@ -1,33 +1,34 @@
 class HttpService {
 
+    _handleErrors(res) {
+        if(!res.ok) throw new Error(res.statusText);
+        return res;
+    }
+
     get(url) {
 
-        return new Promise((resolve, reject) => {
-
-            let xhr = new XMLHttpRequest();
-            xhr.open('GET', url);
-    
-            xhr.onreadystatechange = () => {
-                if(xhr.readyState == 4) {
-                    if(xhr.status == 200) {
-    
-                        resolve(JSON.parse(xhr.responseText));
-                        
-                    } else {
-    
-                        console.log(xhr.responseText);
-                        reject(xhr.responseText);
-                    }
-                }
-            }
-            xhr.send();
-
-        });
+        return fetch(url)
+            .then(res => this._handleErrors(res))
+            .then(res => res.json());
+        
     }
 
     post(url, dado) {
         
+        //NOVA API Fetch do JS
+
+        return fetch(url, {
+            headers: {'Content-type' : 'application/json'},
+            method: 'post',
+            body: JSON.stringify(dado)
+
+        })
+        .then(res => this._handleErrors(res));
+
+    }
         
+        //Codigo usado anteriormente
+        /*
                     return new Promise((resolve, reject) => {
         
                         let xhr = new XMLHttpRequest();
@@ -50,4 +51,5 @@ class HttpService {
                     });
         
                 }
+                */
 }
